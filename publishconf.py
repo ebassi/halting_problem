504fc55ab09dd45aa47838af12d40537d0384960
Emmanuele Bassi <ebassi@gnome.org>
2014-12-13 16:56:11 +0000

    Move to https by default

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.bassi.io'
+SITEURL = 'https://www.bassi.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
